9a5decd4ec3b371219a2c193914095575f22fbfb
Dominik Tichov <dominik.tichov@stud.uni-corvinus.hu>
2021-10-01 16:33:39 +0200

    Create Add function

--- UserMaintenance/Form1.cs
+++ UserMaintenance/Form1.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintenance.Entities;
 
 namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
         public Form1()
         {
             InitializeComponent();
@@ -20,11 +22,26 @@ namespace UserMaintenance
             label2.Text = Resource1.FirstName;
             button1.Text = Resource1.Add;
 
+            // listbox1
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
+
         }
 
         private void label2_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                LastName = textBox1.Text,
+                FirstName = textBox2.Text
+            };
+            users.Add(u);
         }
     }
 }
