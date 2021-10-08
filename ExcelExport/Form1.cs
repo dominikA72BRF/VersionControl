8a0f161f9445e9696d02c93d575981c7fce423b1
Dominik Tichov <dominik.tichov@stud.uni-corvinus.hu>
2021-10-08 15:25:26 +0200

    Adatok kiíratása a fájlba

--- ExcelExport/Form1.cs
+++ ExcelExport/Form1.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
 using System.Reflection;
+using System;
 
 namespace ExcelExport
 {
@@ -11,12 +12,16 @@ namespace ExcelExport
 
         RealEstateEntities1 context = new RealEstateEntities1();
         List<Flat> lakasok;
+        Excel.Application xlApp;
+        Excel.Workbook xlWB; 
+        Excel.Worksheet xlSheet;
 
         public Form1()
         {
             InitializeComponent();
             LoadData();
             dataGridView1.DataSource = lakasok;
+            CreateExcel();
         }
 
         private void LoadData()
@@ -24,7 +29,93 @@ namespace ExcelExport
             lakasok = context.Flats.ToList();
         }
 
+        public void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWB.ActiveSheet;
 
+                CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+        }
+
+        private void CreateTable()
+        {
+            string[] headers = new string[] {
+                "Kód",
+                "Eladó",
+                "Oldal",
+                "Kerület",
+                "Lift",
+                "Szobák száma",
+                "Alapterület (m2)",
+                "Ár (mFt)",
+                "Négyzetméter ár (Ft/m2)"
+            };
+
+            for (int i = 0; i < headers.Length; i++)
+                xlSheet.Cells[1, i + 1] = headers[i];
+
+                object[,] values = new object[lakasok.Count, headers.Length];
+
+                int counter = 0;
+
+                foreach (var lakas in lakasok)
+                {
+                    values[counter, 0] = lakas.Code;
+                values[counter, 1] = lakas.Vendor;
+                values[counter, 2] = lakas.Side;
+                values[counter, 3] = lakas.District;
+                values[counter, 4] = lakas.Elevator;
+                values[counter, 5] = lakas.NumberOfRooms;
+                values[counter, 6] = lakas.FloorArea;
+                values[counter, 7] = lakas.Price;
+                values[counter, 8] = "";
+
+                counter++;
+                }
+
+
+            xlSheet.get_Range(
+                     GetCell(2, 1),
+                     GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+
+
+        }
+
+        private string GetCell(int x, int y)
+            {
+                string ExcelCoordinate = "";
+                int dividend = y;
+                int modulo;
+
+                while (dividend > 0)
+                {
+                    modulo = (dividend - 1) % 26;
+                    ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                    dividend = (int)((dividend - modulo) / 26);
+                }
+                ExcelCoordinate += x.ToString();
+
+                return ExcelCoordinate;
+            }
+
+        
 
     }
 }
