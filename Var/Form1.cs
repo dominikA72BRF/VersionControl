053f931eb47ce4d6f32c4f297abebe0fa2c26afc
Dominik Tichov <dominik.tichov@stud.uni-corvinus.hu>
2021-10-22 19:18:52 +0200

    Portfolio értéke

--- Var/Form1.cs
+++ Var/Form1.cs
@@ -25,6 +25,9 @@ namespace Var
             dataGridView1.DataSource = Ticks;
 
             CreatePortfolio();
+
+            
+
         }
 
         private void CreatePortfolio()
@@ -35,6 +38,21 @@ namespace Var
 
             dataGridView2.DataSource = Portfolio;
         
+        }
+
+        private decimal GetPortfolioValue(DateTime date)
+        {
+            decimal value = 0;
+            foreach (var item in Portfolio)
+            {
+                var last = (from x in Ticks
+                            where item.Index == x.Index.Trim()
+                               && date <= x.TradingDay
+                            select x)
+                            .First();
+                value += (decimal)last.Price * item.Volume;
+            }
+            return value;
         }
 
 
