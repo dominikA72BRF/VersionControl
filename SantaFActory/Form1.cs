7253369f7d15106d64101e06ea908d2b04c22d4d
Dominik Tichov <dominik.tichov@stud.uni-corvinus.hu>
2021-11-12 10:21:19 +0100

    Create futószalag

--- SantaFActory/Form1.cs
+++ SantaFActory/Form1.cs
@@ -14,7 +14,7 @@ namespace SantaFActory
     public partial class Form1 : Form
     {
 
-        List<Ball> balls = new List<Ball>();
+        List<Ball> _balls = new List<Ball>();
 
         private BallFactory _ballFactory;
 
@@ -29,19 +29,35 @@ namespace SantaFActory
         {
             InitializeComponent();
             BallFactory = new BallFactory();
-
             
+         
 
         }
 
         private void createTimer_Tick(object sender, EventArgs e)
         {
-
+            var ball = BallFactory.CreateNew();
+            _balls.Add(ball);
+            ball.Left = -ball.Width;
+            mainPanel.Controls.Add(ball);
         }
 
         private void conveyorTimer_Tick(object sender, EventArgs e)
         {
+            var maxPosition = 0;
+            foreach (var ball in _balls)
+            {
+                ball.MoveBall();
+                if (ball.Left > maxPosition)
+                    maxPosition = ball.Left;
+            }
 
+            if (maxPosition > 1000)
+            {
+                var oldestBall = _balls[0];
+                mainPanel.Controls.Remove(oldestBall);
+                _balls.Remove(oldestBall);
+            }
         }
     }
 }
