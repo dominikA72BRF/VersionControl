8fc48c7ab37dd58e1e36ea40b8f5e1689e023c8b
Dominik Tichov <dominik.tichov@stud.uni-corvinus.hu>
2021-11-12 10:16:28 +0100

    BallFactory

--- SantaFActory/Form1.cs
+++ SantaFActory/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SantaFActory.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,25 @@ namespace SantaFActory
 {
     public partial class Form1 : Form
     {
+
+        List<Ball> balls = new List<Ball>();
+
+        private BallFactory _ballFactory;
+
+        public BallFactory BallFactory
+        {
+            get { return _ballFactory; }
+            set { _ballFactory = value; }
+        }
+
+
         public Form1()
         {
             InitializeComponent();
+            BallFactory = new BallFactory();
+
+            
+
         }
     }
 }
