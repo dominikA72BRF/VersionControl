8437766c5d55496c08c2348ed906260d948e3c9a
Dominik Tichov <dominik.tichov@stud.uni-corvinus.hu>
2021-11-12 10:55:24 +0100

    Car class

---
+++ SantaFActory/Entities/Car.cs
@@ -0,0 +1,20 @@
+﻿using SantaFActory.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SantaFActory.Entities
+{
+    public class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            var image = Image.FromFile(@"Images\car.png");
+            g.DrawImage(image, 0, 0, Width, Height);
+
+        }
+    }
+}
--- SantaFActory/Form1.cs
+++ SantaFActory/Form1.cs
@@ -47,7 +47,7 @@ namespace SantaFActory
             var maxPosition = 0;
             foreach (var ball in _balls)
             {
-                ball.MoveBall();
+                ball.MoveToy();
                 if (ball.Left > maxPosition)
                     maxPosition = ball.Left;
             }
