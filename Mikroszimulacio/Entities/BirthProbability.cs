23b29c6257ed8cda9b6b70956fddf677fad29346
Dominik Tichov <dominik.tichov@stud.uni-corvinus.hu>
2021-12-03 10:07:34 +0100

    Class-ok létrehozása

---
+++ Mikroszimulacio/Entities/BirthProbability.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mikroszimulacio.Entities
+{
+    class BirthProbability
+    {
+        public int Age { get; set; }
+        public int NbrOfChildren { get; set; }
+
+        public double P { get; set; }
+
+
+    }
+}
---
+++ Mikroszimulacio/Entities/Gender.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mikroszimulacio.Entities
+{
+
+    public enum Gender
+    {
+        Male = 1,
+        Female = 2
+    }
+
+    // Példa enumerációs változók használatára
+    Gender ádámNeme = Gender.Male;
+    Gender évaNeme = (Gender)2;
+
+    if (ádámNeme == Gender.Male)
+    {
+     ...
+    }
+
+}
+
