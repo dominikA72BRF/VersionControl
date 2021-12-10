bf5a779e3b9b78e84242e2695deb3664cc4e0ca9
Dominik Tichov <dominik.tichov@stud.uni-corvinus.hu>
2021-12-10 10:08:41 +0100

    Játéktér

--- Evolucio/Form1.Designer.cs
+++ Evolucio/Form1.Designer.cs
@@ -35,7 +35,7 @@ namespace Evolucio
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(637, 319);
+            this.ClientSize = new System.Drawing.Size(693, 366);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
